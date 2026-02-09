66d558cc36c7e94eff408c6c7dc094b83c2f877b
JaHeed21 <jahidrayhan100@gmail.com>
2026-02-09 10:39:13 +0600

    refactor the notFound handler

--- handlers/routeHandlers/notFoundHandler.js
+++ handlers/routeHandlers/notFoundHandler.js
@@ -1,6 +1,6 @@
 /*
  * Title: Not Found Handler
- * Description: Not Found Handler
+ * Description: 404 Not Found Handler
  * Author: Jahid Rayhan
  * Date: 09/02/2026
  */
@@ -8,8 +8,8 @@
 // module scafolding
 const handler = {};
 
-handler.notFoundHandler = (req, res) => {
-  res.end("Not Found");
+handler.notFoundHandler = (requestProperties, callback) => {
+  callback(404, { message: "Not Found" });
 }
 
 module.exports = handler;
